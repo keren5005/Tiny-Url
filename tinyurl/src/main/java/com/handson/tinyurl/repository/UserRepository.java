fd57fb37b5c8bbf36a5898c95530b5544b32d257
Keren Cohen <kerenco2@mta.ac.il>
2024-11-03 17:05:40 +0200

    initialize

---
+++ tinyurl/src/main/java/com/handson/tinyurl/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.handson.tinyurl.repository;
+
+import com.handson.tinyurl.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    User findFirstByName(String name);
+
+}
+
